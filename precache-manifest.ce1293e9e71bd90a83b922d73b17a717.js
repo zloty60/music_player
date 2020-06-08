dc53ddcca7a2c500ab311759cd5119900f68567f
Przemek <przemek.bzowka@gmail.com>
2020-06-08 21:15:42 +0200

    Updates

---
+++ precache-manifest.ce1293e9e71bd90a83b922d73b17a717.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f8d60be085445380ce80cdfe040328",
+    "url": "/music_player/index.html"
+  },
+  {
+    "revision": "68687725cc24727d7e64",
+    "url": "/music_player/static/css/2.9db57ac0.chunk.css"
+  },
+  {
+    "revision": "8fe07b17a82eec4d076c",
+    "url": "/music_player/static/css/main.7b6cedd4.chunk.css"
+  },
+  {
+    "revision": "68687725cc24727d7e64",
+    "url": "/music_player/static/js/2.03e3351c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/music_player/static/js/2.03e3351c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe07b17a82eec4d076c",
+    "url": "/music_player/static/js/main.5b1b7837.chunk.js"
+  },
+  {
+    "revision": "ee781bef63470d604ead",
+    "url": "/music_player/static/js/runtime-main.d6d2191a.js"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/music_player/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/music_player/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "70f2e66c0be140a99599142e476e15f5",
+    "url": "/music_player/static/media/Play_active.70f2e66c.png"
+  },
+  {
+    "revision": "cb35493830042c095ea4367b4f65aee7",
+    "url": "/music_player/static/media/Play_inactive.cb354938.png"
+  },
+  {
+    "revision": "6d8b10090438264aa26fc7631331c427",
+    "url": "/music_player/static/media/back_ico.6d8b1009.svg"
+  },
+  {
+    "revision": "2b81866b8c960b0a9bdb5929f0f010a6",
+    "url": "/music_player/static/media/cover-1.2b81866b.png"
+  },
+  {
+    "revision": "45551aef094db92b6cc44caa325076b3",
+    "url": "/music_player/static/media/cover.45551aef.png"
+  },
+  {
+    "revision": "efd35a48d3758a96543e17f5e6d24850",
+    "url": "/music_player/static/media/hide_ico.efd35a48.svg"
+  },
+  {
+    "revision": "cc63a9cfe22015dee430f667e13d9d10",
+    "url": "/music_player/static/media/more_ico.cc63a9cf.svg"
+  },
+  {
+    "revision": "908e77adf0762e3ca48d8da50ad268d9",
+    "url": "/music_player/static/media/next_ico.908e77ad.svg"
+  },
+  {
+    "revision": "d560ea63609f1bd760297025444ffa79",
+    "url": "/music_player/static/media/playlist_ico.d560ea63.svg"
+  },
+  {
+    "revision": "61530e1d25eb1ef8ad5d7e15af9ac18a",
+    "url": "/music_player/static/media/previous_ico.61530e1d.svg"
+  },
+  {
+    "revision": "f24e65322df49b5a8fd3c450d993a60d",
+    "url": "/music_player/static/media/repeat_ico.f24e6532.svg"
+  },
+  {
+    "revision": "9cca0f11fe187d5900f978490e200dd4",
+    "url": "/music_player/static/media/shuffle_ico.9cca0f11.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/music_player/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/music_player/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/music_player/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/music_player/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a21957684324528264df44116fc5c48c",
+    "url": "/music_player/static/media/unreleased_cover.a2195768.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d04fb45e06674e096b986d87daf8fc39.js"
+  "/music_player/precache-manifest.ce1293e9e71bd90a83b922d73b17a717.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/music_player/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
